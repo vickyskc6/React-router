0bdc5dedd705dabe1a1ff793aee78ede5bad36b0
Gokulraj210 <gokulrajgandhi99@gmail.com>
2023-04-05 12:54:41 +0530

    router course is complete

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 import React from "react";
-import {Route, Routes,Link} from 'react-router-dom'
+import {Route, Routes,Link, NavLink} from 'react-router-dom'
 import { Home } from "./pages/home";
 import { About } from "./pages/about";
-// import { Content } from "./pages/content";
+import { Content } from "./pages/content";
 import { BookList } from "./pages/BookList";
 import {Book} from './pages/Book'
-
+import {NotFount} from './pages/Navigate'
+ 
 function App() {
   return (
     <div className="App" style={{display:"flex",justifyContent:"center"}} >
        <nav>
           <ul>
-          <li> <Link to='/'>Home </Link></li>
+          <li> 
+             <NavLink
+                
+            to='/home' >
+                Home
+             </NavLink>
+
+           
+          </li>
           <li> <Link to='/about'>About </Link></li>
-          <li> <Link to='/bookList'>BookList </Link></li>
+          <li> <Link to='/book'>book list</Link></li>       
+                 
           </ul>
        </nav>
  
 
        <Routes >
+         {/* <Route path="/book" element={<h1>router concept</h1>} /> */}
         <Route path ="/" element= {<Home />} />  
         <Route path ="/about" element= {<About/>} />  
-        <Route path ="/bookList" element= {<BookList/>} />  
-        <Route path ="/Book" element= {<Book/>} />  
-  
+        <Route path ="/book" element= {<BookList/>} />  
+        <Route path ="/book/:id" element= {<Book/>} />
+        <Route path ="/content/new" element= {<Content/>} />
+        <Route path ="*" element= {<NotFount/>} />
+          
+         
         </Routes>   
 
     </div>
--- src/pages/BookList.js
+++ src/pages/BookList.js
@@ -1,4 +1,17 @@
-export function BookList(){
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 
-return <h1>book list</h1>
+export function BookList(){
+  const [seachParams,getSerachParams] = useSearchParams({n:3})
+     const number =  seachParams.get("n")
+      const location = useLocation()
+      console.log(location)
+
+return <>
+<h1>book list</h1>
+ <Link to="/book/1">Book 2</Link>
+ <Link to={`/book/${number}`}>Book 1</Link>
+   <input type="number" value={number} onChange={(e)=> {getSerachParams({n:e.target.value})}}></input>
+   {location.state}
+</>
+
 }
